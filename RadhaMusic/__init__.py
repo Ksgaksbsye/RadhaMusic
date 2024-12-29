2ef5f3c4eff6dbdf472a0b318ba797adce1ec94b
Ksgaksbsye <naincyprajapati122008@gmail.com>
2024-12-29 12:00:03 +0530

    Create __init__.py

---
+++ RadhaMusic/__init__.py
@@ -0,0 +1,26 @@
+from RadhaMusic.core.bot import AMBOTOP
+from RadhaMusic.core.dir import dirr
+from RadhaMusic.core.git import git
+from RadhaMusic.core.userbot import Userbot
+from RadhaMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = AMBOTOP()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
