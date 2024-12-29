e403e0578099095d00a2d6d899d53a137de34972
Ksgaksbsye <naincyprajapati122008@gmail.com>
2024-12-29 19:34:18 +0530

    Update admins.py

--- RadhaMusic/utils/decorators/admins.py
+++ RadhaMusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from RessoMusic import app
-from RessoMusic.misc import SUDOERS, db
-from RessoMusic.utils.database import (
+from RadhaMusic import app
+from RadhaMusic.misc import SUDOERS, db
+from RadhaMusic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
