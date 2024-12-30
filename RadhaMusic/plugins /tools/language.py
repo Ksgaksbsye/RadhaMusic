803c4572899b4d26faf80cef110cea8be207481c
Ksgaksbsye <naincyprajapati122008@gmail.com>
2024-12-30 17:50:43 +0530

    Update language.py

--- RadhaMusic/plugins /tools/language.py
+++ RadhaMusic/plugins /tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from RessoMusic import app
-from RessoMusic.utils.database import get_lang, set_lang
-from RessoMusic.utils.decorators import ActualAdminCB, language, languageCB
+from RadhaMusic import app
+from RadhaMusic.utils.database import get_lang, set_lang
+from RadhaMusic.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
