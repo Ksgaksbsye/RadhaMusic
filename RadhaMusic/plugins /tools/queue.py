7597add782a455e7f1386e17e5aec32807d1a0fa
Ksgaksbsye <naincyprajapati122008@gmail.com>
2024-12-30 17:56:04 +0530

    Update queue.py

--- RadhaMusic/plugins /tools/queue.py
+++ RadhaMusic/plugins /tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from RessoMusic import app
-from RessoMusic.misc import db
-from RessoMusic.utils import AMBOTOPBin, get_channeplayCB, seconds_to_min
-from RessoMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from RessoMusic.utils.decorators.language import language, languageCB
-from RessoMusic.utils.inline import queue_back_markup, queue_markup
+from RadhaMusic import app
+from RadhaMusic.misc import db
+from RadhaMusic.utils import AMBOTOPBin, get_channeplayCB, seconds_to_min
+from RadhaMusic.utils.database import get_cmode, is_active_chat, is_music_playing
+from RadhaMusic.utils.decorators.language import language, languageCB
+from RadhaMusic.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
