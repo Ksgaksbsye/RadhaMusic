8104d892d3c951a2912ed9465ae2a57a913e596c
Ksgaksbsye <naincyprajapati122008@gmail.com>
2024-12-31 09:25:11 +0530

    Update pause.py

--- RadhaMusic/plugins /admins/pause.py
+++ RadhaMusic/plugins /admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from RessoMusic import app
-from RessoMusic.core.call import AMBOTOP
-from RessoMusic.utils.database import is_music_playing, music_off
-from RessoMusic.utils.decorators import AdminRightsCheck
-from RessoMusic.utils.inline import close_markup
+from RadhaMusic import app
+from RadhaMusic.core.call import AMBOTOP
+from RadhaMusic.utils.database import is_music_playing, music_off
+from RadhaMusic.utils.decorators import AdminRightsCheck
+from RadhaMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
